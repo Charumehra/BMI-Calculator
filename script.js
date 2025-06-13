e43edc6c40883febc4d54239b38cb7ed1d133765
Charumehra <Charumehracm2004@gmail.com>
2025-06-13 13:38:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const height= parseInt(document.getElementById('height').value);
+    const weight= parseInt(document.getElementById('weight').value);
+    const results= document.querySelector('.results');
+
+    if(height === '' || height < 0 || isNaN(height)){
+        results.innerHTML = 'Please enter a valid height';
+        return;
+    }
+    if(weight === '' || weight < 0 || isNaN(weight)){
+        results.innerHTML = 'Please enter a valid weight';
+        return;
+    }
+    const bmi = (weight/((height*height)/10000)).toFixed(1);
+    results.innerHTML = <span>`Your BMI is ${bmi}`</span>;
+})
