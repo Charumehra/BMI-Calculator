5aae257c431d5965a7e1930d2ca538edea117926
Charumehra <Charumehracm2004@gmail.com>
2025-08-05 11:03:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+
 const form = document.querySelector('form');
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    const height= parseInt(document.getElementById('height').value);
-    const weight= parseInt(document.getElementById('weight').value);
-    const results= document.querySelector('.results');
+
+    const height = parseInt(document.getElementById('height').value);
+    const weight = parseInt(document.getElementById('weight').value);
+    const results = document.querySelector('.results');
 
     if(height === '' || height < 0 || isNaN(height)){
         results.innerHTML = 'Please enter a valid height';
         return;
     }
+
     if(weight === '' || weight < 0 || isNaN(weight)){
         results.innerHTML = 'Please enter a valid weight';
         return;
     }
-    const bmi = (weight/((height*height)/10000)).toFixed(1);
-    results.innerHTML = <span>`Your BMI is ${bmi}`</span>;
-})
+
+    const bmi = (weight / ((height * height) / 10000)).toFixed(1);
+    results.innerHTML = `<span>Your BMI is ${bmi}</span>`;
+});
